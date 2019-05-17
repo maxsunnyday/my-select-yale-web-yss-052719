a88427b8217b7e7d65ae4d15ac70d5a528be64c3
Max Sun <max.sun@yale.edu>
2019-05-17 04:17:58 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    new_array = []
+    while i < collection.length
+      thing = yield collection[i]
+      if thing == true
+        new_array << thing
+        i += 1
+      else
+        i += 1
+    end
+  end
 end
