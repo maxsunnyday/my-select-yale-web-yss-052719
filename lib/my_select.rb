314f839e0019b431f34a626340fbc71007e2af7f
Max Sun <max.sun@yale.edu>
2019-05-17 04:19:43 +0000

    My select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    new_array = []
+    while i < collection.length
+      thing = yield collection[i]
+      if thing == true
+        new_array << collection[i]
+        i += 1
+      else
+        i += 1
+      end
+    end
+    new_array
+  end
 end
